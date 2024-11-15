3c3bd513ccb164aaaced7edb9b2bb6581822b77b
Arafat2342 <jashoresinger@gmail.com>
2024-11-15 09:04:17 +0600

    sin

--- form.js
+++ form.js
@@ -1,30 +1,27 @@
-function validate() {
-    event.preventDefault();
+function validate(){
+
+    event.preventDefault()
 
     const email = document.getElementById("email").value;
     const password = document.getElementById("password").value;
     const age = document.getElementById("age").value;
     const msg = document.getElementById("message");
 
-    let message = '';  
+let message ='';
 
-    if (email === '') {
-        message ="Please enter a valid email address.";
-        msg.style.color = 'red';
-    } 
-    else if (password === '') {
-        message = "Password must be at least 8 characters long.";
-        msg.style.color = 'red';
-    } 
-    else if (age === '') {
-        message = "Age must be between 6 and 60.";
-        msg.style.color = 'red';
-    } 
-    else {
-        message = "Sign-in successful!";
-        msg.style.color = 'green';
-    }
+if (email === '') {
+     message = "Wrong username or email.";
+     msg.style.color =' red ';
+} else if (password === '') {
+    message = "invalid password.";
+    msg.style.color =' red ';
+}else if (age === '') {
+    message = "invalid age.";
+    msg.style.color =' red ';
+} else {
+    message = "Succesful."
+    msg.style.color = "green";
+ }
 
-    msg.innerText = message;
-
+ msg.innerText = message;
 }
